0f9e8eb49dae8c5ef023a77c6d7f0ad22225fcf0
shankar <shankar.l5252@gmail.com>
2026-02-14 06:52:31 +0530

    refactor: separate Link from combined import statement

--- client/src/pages/Settings.jsx
+++ client/src/pages/Settings.jsx
@@ -1,4 +1,5 @@
-import { useState, Link } from "react-router-dom";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import { useSettings } from "../hooks/useSettings";
 import FullPageLoader from "../components/FullPageLoader";
 import Dialog from "../components/Dialog";
